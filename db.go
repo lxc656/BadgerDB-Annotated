dgraph-io/badger

Make value log GC pick files before on-disk head pointer.
We should only be GCing log files, which fall before the value log head
pointer stored on disk in LSM (level 0 and onwards), not memtable.

This change introduces an additional argument representing the head
pointer that is passed through into the GC related methods, and then
used to compare and pick a log file in the valueLog.pickLog() method.

Fixes #265.

db.go
@@ -996,5 +996,17 @@ func (db *DB) RunValueLogGC(discardRatio float64) error {
 	if discardRatio >= 1.0 || discardRatio <= 0.0 {
 		return ErrInvalidRequest
 	}
-	return db.vlog.runGC(discardRatio)
+
+	headKey := y.KeyWithTs(head, math.MaxUint64)
+	// Need to pass with timestamp, lsm get removes the last 8 bytes and compares key
+	headValStruct, err := db.lc.get(headKey)
+	if err != nil {
+		return errors.Wrap(err, "Retrieving head from on-disk LSM")
+	}
+
+	var headValPointer valuePointer
+	if len(headValStruct.Value) > 0 {
+		headValPointer.Decode(headValStruct.Value)
+	}
+	return db.vlog.runGC(discardRatio, headValPointer)
 }

lxc656/BadgerDB-Annotated

db.go
@@ -997,5 +997,17 @@ func (db *DB) RunValueLogGC(discardRatio float64) error {
 	if discardRatio >= 1.0 || discardRatio <= 0.0 {
 		return ErrInvalidRequest
 	}
-	return db.vlog.runGC(discardRatio)
+
+	headKey := y.KeyWithTs(head, math.MaxUint64)
+	// Need to pass with timestamp, lsm get removes the last 8 bytes and compares key
+	val, err := db.lc.get(headKey)
+	if err != nil {
+		return errors.Wrap(err, "Retrieving head from on-disk LSM")
+	}
+
+	var head valuePointer
+	if len(val.Value) > 0 {
+		head.Decode(val.Value)
+	}
+	return db.vlog.runGC(discardRatio, head)
 }